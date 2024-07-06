93840412ccd9a84d85914ed5794387732aa97081
castledragon <starlight1532@gmail.com>
2024-07-06 20:40:54 +0900

    ee

--- src/main/java/com/bod/tis/controller/HomeController.java
+++ src/main/java/com/bod/tis/controller/HomeController.java
@@ -1,11 +1,8 @@
 package com.bod.tis.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -17,13 +14,10 @@ public class HomeController {
 		return "index";  // index.html 파일을 찾음
 	}
 
-	@GetMapping("/test2")
+	@GetMapping("/test2	")
 	public String test2() {
-		return "test";  // index.html 파일을 찾음efwfew
+		return "test2";  //
 	}
 
-	@RequestMapping("/error")
-	public String error(HttpServletRequest request) {
-		return "에러 페이지입니다";
-	}
+
 }
