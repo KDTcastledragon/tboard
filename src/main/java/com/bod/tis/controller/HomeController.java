d0398fc40df5f7e7a21b56f4d8807b37b97676ce
castledragon <starlight1532@gmail.com>
2024-07-06 18:26:58 +0900

    fff

--- src/main/java/com/bod/tis/controller/HomeController.java
+++ src/main/java/com/bod/tis/controller/HomeController.java
@@ -1,8 +1,11 @@
 package com.bod.tis.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -12,5 +15,15 @@ public class HomeController {
 	public String home(Model model) {
 		model.addAttribute("message", "Hello, Thymeleaf!");
 		return "index";  // index.html 파일을 찾음
+	}
+
+	@GetMapping("/test2")
+	public String test2() {
+		return "test";  // index.html 파일을 찾음efwfew
+	}
+
+	@RequestMapping("/error")
+	public String error(HttpServletRequest request) {
+		return "에러 페이지입니다";
 	}
 }
