daca2afdc1b19e6b475da227749dd347def50367
castledragon <starlight1532@gmail.com>
2024-07-06 12:42:37 +0900

    rebuilding

--- src/main/java/com/bod/tis/controller/HomeController.java
+++ src/main/java/com/bod/tis/controller/HomeController.java
@@ -1,15 +1,16 @@
 package com.bod.tis.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+
 	@GetMapping("/")
-	public String home() {
-		return "board";
+	public String home(Model model) {
+		model.addAttribute("message", "Hello, Thymeleaf!");
+		return "index";  // index.html 파일을 찾음
 	}
-
-
 }
